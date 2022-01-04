07f5fb5ca4f490fd8009a3ca6a066dd510f94094
chinwei.hsu <dacerwei@gmail.com>
2022-01-04 18:35:51 +0800

    add text break

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -7,11 +7,11 @@ const Card = (props: Props) => {
   const { title, content } = props;
   return (
     <div className="bg-light rounded p-2">
-      <h2 className="text-start">
+      <h2 className="text-start text-break">
         {title}
       </h2>
       <div className="p-2">
-        <p className="text-start">
+        <p className="text-start text-break">
           {content}
         </p>
       </div>
